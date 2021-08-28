6a910573102a2530efb2f9a5c30285d1410062fc
hernandez87v <hernandez87v@hotmail.com>
2021-08-28 14:39:06 -0700

    add user interaction button

--- src/App.fs
+++ src/App.fs
@@ -1,3 +1,9 @@
 module App
 
-printfn "Hello from Fable"
+open Browser.Dom
+
+let printMsgButton = document.getElementById "printMsg"
+
+printMsgButton.onclick <- fun eventArgs -> printfn "Button clicked"
+
+printfn "Fable is up and running..."
