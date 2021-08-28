1c60e49c3a5d5b59bdd6e79eac0b7e227a9ecd5c
hernandez87v <hernandez87v@hotmail.com>
2021-08-28 15:10:42 -0700

    add .NET code System.Random to randomize increase/decrease of counter

--- src/App.fs
+++ src/App.fs
@@ -9,18 +9,20 @@ let countViewer = document.getElementById "countViewer"
 
 let mutable currentCount = 0
 
+let rnd = System.Random()
+
 //attach even handlers
 increase.onclick <-
     fun ev ->
         // update the state
-        currentCount <- currentCount + 1
+        currentCount <- currentCount + rnd.Next(5, 10)
         // update the view
         countViewer.innerText <- sprintf "Count is at %d" currentCount
 
 decrease.onclick <-
     fun ev ->
         // update the state
-        currentCount <- currentCount - 1
+        currentCount <- currentCount - rnd.Next(5, 10)
         // update the view
         countViewer.innerText <- sprintf "Count is at %d" currentCount
 
